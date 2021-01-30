a03f9cbcb97228a07e43f198800fe30c465b35d2
bny1485 <benyaminmahmoudyan@gmail.com>
2021-01-30 14:36:36 +0330

    form

--- ablog/theblog/views.py
+++ ablog/theblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 
 # def home(request):
@@ -19,4 +20,6 @@ class ArticleDetailView(DetailView):
 class AddPostView(CreateView):
     model = Post
     template_name = 'add_post.html'
-    fields = '__all__'
+    form_class = PostForm
+    # fields = '__all__'
+    # fields = ('title', 'body')
