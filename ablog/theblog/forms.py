aec99db2849572e3a77238b6c324f89290de95b9
bny1485 <benyaminmahmoudyan@gmail.com>
2021-02-04 16:39:42 +0330

    Add Article Snippets

--- ablog/theblog/forms.py
+++ ablog/theblog/forms.py
@@ -15,12 +15,13 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'category', 'author', 'body')
+        fields = ('title', 'title_tag', 'category', 'author', 'body', 'snippet')
         widget = {
             'title': forms.TextInput(attrs={'class': 'form-control', }),
             'title_tag': forms.TextInput(attrs={'class': 'form-control', }),
             'author': forms.Select(attrs={'class': 'form-control', }),
             'body': forms.Textarea(attrs={'class': 'form-control', }),
+            'snippet': forms.Textarea(attrs={'class': 'form-control', }),
         }
 
 
