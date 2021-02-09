45b0ca37746a7f63300dc2a44d0a5474574a1c3e
bny1485 <benyaminmahmoudyan@gmail.com>
2021-02-09 13:08:06 +0330

    Edit Profile Page Form

--- ablog/members/urls.py
+++ ablog/members/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import UserRegisterView, UserEditView, PasswordsChangeView, password_success, ShowProfilePageView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, password_success, ShowProfilePageView, EditProfilePageView
 from django.contrib.auth import views as auth_views
 
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('password/', PasswordsChangeView.as_view()),
     path('password_success', password_success, name='password_success'),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(), name="show_profile_page"),
+    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name="edit_profile_page"),
 ]
--- ablog/members/views.py
+++ ablog/members/views.py
@@ -7,12 +7,21 @@ from .forms import SignUpForm, EditProfileForm
 from theblog.models import Profile
 
 
+class EditProfilePageView(generic.UpdateView):
+    model = Profile
+    template_name = 'registration/edit_profile_page.html'
+    fields = ['bio', 'profile_pic', 'website_url',
+              'website_url', 'telegram_url', 'instagram_url']
+    success_url = reverse_lazy('login')
+
+
 class ShowProfilePageView(generic.DetailView):
     model = Profile
     template_name = 'registration/user_profile.html'
 
     def get_context_data(self, *args, **kwargs):
-        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+        context = super(ShowProfilePageView,
+                        self).get_context_data(*args, **kwargs)
         page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
         context['page_user'] = page_user
         return context
