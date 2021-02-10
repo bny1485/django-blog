e609c17eabbebbec38571b32c9363d9e9b8d4ba9
bny1485 <benyaminmahmoudyan@gmail.com>
2021-02-10 12:34:54 +0330

    Style The Login Page With Bootstrap

--- ablog/members/urls.py
+++ ablog/members/urls.py
@@ -9,9 +9,12 @@ urlpatterns = [
     # path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
     path('password/', PasswordsChangeView.as_view()),
     path('password_success', password_success, name='password_success'),
-    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name="show_profile_page"),
-    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name="edit_profile_page"),
-    path('create_profile_page/', CreateProfilePageView.as_view(), name="create_profile_page"),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(),
+         name="show_profile_page"),
+    path('<int:pk>/edit_profile_page/',
+         EditProfilePageView.as_view(), name="edit_profile_page"),
+    path('create_profile_page/', CreateProfilePageView.as_view(),
+         name="create_profile_page"),
 
-    
+
 ]
